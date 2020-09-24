e37e9f56fbaccebb14f0593693ea7518cbc976f0
jevdjauon <aleksandar@uon.rs>
2020-09-24 13:36:08 +0200

    gists list

--- App.js
+++ App.js
@@ -1,16 +1,136 @@
-import * as React from "react";
-import { View, Text } from "react-native";
+import React, { Component } from "react";
+import { FlatList, Image } from "react-native";
+import {
+  Header,
+  H1,
+  GistsRow,
+  GistsImage,
+  GistsFile,
+  Main,
+  Separator,
+  OpenImgComponent,
+  ModalContainer,
+  Mask,
+  ModalInside,
+  ModalItem,
+} from "./styles";
 
-export default function App() {
-  return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Universal React with Expo</Text>
-    </View>
-  );
+export default class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      data: [],
+      // imgOpen: false,
+      activeUri: "",
+      visible: false,
+    };
+  }
+
+  componentDidMount() {
+    this._getGists();
+  }
+
+  onRefresh() {
+    this._getGists();
+  }
+
+  _getGists() {
+    fetch("https://api.github.com/gists/public")
+      .then((response) => response.json())
+      .then((json) => {
+        this.setState({
+          data: json,
+        });
+      })
+
+      .catch((error) => console.error(error));
+  }
+
+  pressHandler = (id, imgUri) => {
+    this.setState({
+      activeUri: imgUri,
+      visible: true,
+    });
+    setTimeout(() => {
+      this.setState({
+        visible: false,
+        activeUri: "",
+      });
+    }, 1000);
+  };
+
+  render() {
+    const { data, visible } = this.state;
+
+    const Item = ({ file, imgUri, id }) => (
+      <GistsRow onPress={() => this.pressHandler(id, imgUri)}>
+        <GistsImage
+          source={{
+            uri: imgUri,
+          }}
+        />
+        <GistsFile>{file}</GistsFile>
+      </GistsRow>
+    );
+
+    const ImageComponent = (props) => (
+      <OpenImgComponent>
+        <Image
+          style={{ width: 300, height: 300 }}
+          source={{
+            uri: props.uri,
+          }}
+        />
+      </OpenImgComponent>
+    );
+
+    const renderItem = ({ item }) => (
+      <Item
+        id={item.id}
+        imgUri={item.owner.avatar_url}
+        file={Object.getOwnPropertyNames(item.files)
+          .toString()
+          .substring(0, 100)}
+      />
+    );
+
+    return (
+      <Main>
+        <Header>
+          <H1>Gists</H1>
+        </Header>
+        <FlatList
+          style={{ height: "100%" }}
+          data={data}
+          renderItem={renderItem}
+          keyExtractor={(item) => item.id}
+          ItemSeparatorComponent={Separator}
+          initialNumToRender={10}
+          onEndReachedThreshold={0}
+          maxToRenderPerBatch={1}
+          windowSize={10}
+        />
+        <ModalContainer
+          visible={visible}
+          animationType="fade"
+          transparent={true}
+        >
+          <Mask />
+          <ModalInside>
+            <ModalItem
+              onPress={() =>
+                this.setState({
+                  visible: false,
+                  activeUri: "",
+                })
+              }
+            >
+              <ImageComponent uri={this.state.activeUri} />
+            </ModalItem>
+          </ModalInside>
+        </ModalContainer>
+      </Main>
+    );
+  }
 }
---
+++ styles/index.js
@@ -0,0 +1,86 @@
+import styled, { css } from "styled-components/native";
+
+export const Main = styled.View`
+  height: 100%;
+  background-color: #94958b;
+`;
+
+export const Header = styled.View`
+  margin-top: 25px;
+  height: 30px;
+  background-color: purple;
+  flex-direction: row;
+  justify-content: flex-start;
+  align-items: center;
+  margin-bottom: 5px;
+`;
+
+export const H1 = styled.Text`
+  font-size: 16px;
+  font-weight: bold;
+  color: black;
+  text-transform: uppercase;
+  margin-left: 10px;
+`;
+
+export const GistsRow = styled.TouchableOpacity`
+  flex-direction: row;
+  justify-content: flex-start;
+  align-items: center;
+  height: 50px;
+`;
+
+export const GistsImage = styled.Image`
+  width: 40px;
+  height: 40px;
+  margin: 10px;
+  border-radius: 5px;
+  background-color: white;
+`;
+
+export const GistsFile = styled.Text`
+  font-size: 15px;
+  color: black;
+  margin: 0 10px;
+  max-width: 70%;
+`;
+
+export const Separator = styled.View`
+  height: 1px;
+  width: 100%;
+  background-color: gray;
+`;
+
+export const OpenImgComponent = styled.View``;
+
+export const ModalContainer = styled.Modal`
+  position: relative;
+  width: 100%;
+  height: 100%;
+`;
+
+export const Mask = styled.View`
+  position: absolute;
+  top: 0;
+  right: 0;
+  bottom: 0;
+  left: 0;
+  z-index: 100;
+  background-color: rgba(0, 0, 0, 0.5);
+`;
+
+export const ModalInside = styled.View`
+  z-index: 200;
+  height: 100%;
+  width: 100%;
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  align-items: center;
+`;
+
+export const ModalItem = styled.TouchableOpacity`
+  padding: 10px;
+  justify-content: center;
+  align-items: center;
+`;
